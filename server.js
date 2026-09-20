393b02802dacacd0eab6c81fd96c4f6b12276177
Alexander Amos <alexanderba2019@gmail.com>
2026-09-20 00:38:49 -1100

    feat: add 404 and global error handling + gitignore - Member 8

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ app.get('/', (req, res) => {
     res.send('Group 4 Notes API is running...');
 });
 
+// 404 Handler - for routes that don't exist
+app.use((req, res) => {
+    res.status(404).json({ message: "Route not found" });
+});
+
+// General error handler
+app.use((err, req, res, next) => {
+    console.error(err.stacks);
+    res.status(500).json({ "message: Something went wrong!"});
+});
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
