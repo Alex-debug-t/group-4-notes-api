981649b21e01c76da6d326181b824e44d7e8d04c
Alexander Amos <alexanderba2019@gmail.com>
2026-09-19 15:42:09 -1100

    feat: setup express server with json middleware - member 2

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-// server file
+const express = require('express');
+const app = express();
+// THIS IS THE IMPORTANT PART - The middleware
+app.use(express.json()); // This allows your API to read JSON body like {tittle: "Hello World"}
+
+app.get('/', (req, res) => {
+    res.send('Group 4 Notes API is running...');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
